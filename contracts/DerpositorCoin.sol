dd29b696a846bf831a71674455b9a0e126e28db7
Luciano Ramello <luchosr@gmail.com>
2022-10-05 11:37:12 +0200

    update contracts relationships

--- contracts/DerpositorCoin.sol
+++ contracts/DerpositorCoin.sol
@@ -2,7 +2,10 @@
 pragma solidity 0.8.17;
 import {ERC20} from "./ERC20.sol";
 
-contract DepositorCoin is ERC20{
-constructor() ERC("DepositorCoin", "DPC"){}
+contract DepositorCoin is ERC20 {
+    address public owner;
 
+    constructor() ERC("DepositorCoin", "DPC") {
+        owner = msg.sender;
+    }
 }
