54d6bc0dc87a072f15986e4cdb4c3bf03589e8c2
Luciano Ramello <luchosr@gmail.com>
2022-10-05 15:16:57 +0200

    update mint and burn functions

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -4,5 +4,39 @@ import {ERC20} from "./ERC20.sol";
 import {DepositorCoin} from "./DerpositorCoin.sol";
 
 contract StableCoin is ERC20 {
-    constructor() ERC("StablerCoin", "STC") {}
+    DepositorCoin public depositorCoin;
+
+    uint256 private constant ETH_IN_USD_PRICE = 2000;
+    uint256 public feeRatePercentage;
+
+    constructor(uint256 _feeRatePercentage) ERC("StablerCoin", "STC") {
+      feeRatePercentage = _feeRatePercentage;
+    }
+
+    function mint() external payable {
+        uint256 fee = _getFee(msg.value);
+        uint256 remainingEth = msg.value - fee;
+
+        uint256 mintStableCounAmount = remainingEth * ETH_IN_USD_PRICE;
+        _mint(msg.sender, mintStableCounAmount);
+    }
+
+    function burn(uint256 burnStableCoinAmount) external{
+      _burn(msg.sender, burnStableCoinAmount);
+      
+      uint256 refundingEth = burnStableCoinAmount / ETH_IN_USD_PRICE;
+      uint256 fee = _getFee(refundingEth);
+      uint256 remainingRefundingEth = refundingEth - fee;
+
+      (bool success,) = msg.sender.call{value:remainingRefundingEth}("");
+      require(success, "STC: Burn refund transaction failed");
+    }
+
+    function _getFee (uint256 ethAmount)private view returns (uint256){
+      bool hasDepositors = address(depositorCoin) 1= address(0) && depositorCoin.totalSupply() > 0;
+      if(!hasDepositors){
+        return 0;
+      }
+      return (feeRatePercentage * ethAmount) / 100;
+    }
 }
