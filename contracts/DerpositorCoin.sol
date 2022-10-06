4ccff8f6c75abb048a57dfe5195b6c4fe1339dfa
Luciano Ramello <luchosr@gmail.com>
2022-10-06 13:30:17 +0200

    fix typos

--- contracts/DerpositorCoin.sol
+++ contracts/DerpositorCoin.sol
@@ -5,17 +5,17 @@ import {ERC20} from "./ERC20.sol";
 contract DepositorCoin is ERC20 {
     address public owner;
 
-    constructor() ERC("DepositorCoin", "DPC") {
+    constructor() ERC20("DepositorCoin", "DPC") {
         owner = msg.sender;
     }
 
     function mint(address to, uint256 amount) external {
-        require(msg.msg.sender == owner, "DPC: Only owner can mint");
+        require(msg.sender == owner, "DPC: Only owner can mint");
         _mint(to, amount);
     }
 
     function burn(address from, uint256 amount) external {
-        require(msg.msg.sender == owner, "DPC: Only owner can burn");
+        require(msg.sender == owner, "DPC: Only owner can burn");
         _burn(from, amount);
     }
 }
